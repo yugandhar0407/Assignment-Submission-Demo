93ae1ef3e01910c588295963333891f25e4e367f
yugandhar0407 <129308425+yugandhar0407@users.noreply.github.com>
2023-03-31 17:21:17 +0530

    Day 3 Assignment Submission

---
+++ App.java
@@ -0,0 +1,18 @@
+package onlinelearner;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		int num = 99;
+
+		if (num > 0) {
+			System.out.println("Positive Number");
+		} else if (num < 0) {
+			System.out.println("Negitave Number");
+		} else {
+			System.out.println("Number is Zero");
+		}
+	}
+
+}
